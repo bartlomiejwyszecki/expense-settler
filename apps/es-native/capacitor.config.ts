5cd03d338ce965dfe522455175d9edccacee8abc
bartlomiejwyszecki <68397602+bartlomiejwyszecki@users.noreply.github.com>
2023-03-15 21:53:00 +0100

    Ionic app initialized (#3)

---
+++ apps/es-native/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'es-native',
+  webDir: '../../dist/apps/es-native',
+  bundledWebRuntime: false,
+};
+
+export default config;
