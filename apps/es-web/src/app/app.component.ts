a530929c77ef0225c65020db3d781310660ee455
bartlomiejwyszecki <68397602+bartlomiejwyszecki@users.noreply.github.com>
2023-03-17 00:01:59 +0100

    Lib task init models (#5)
    * Shared lib added (#1)
    
    * Add user interface
    
    * Added user interfaces export

--- apps/es-web/src/app/app.component.ts
+++ apps/es-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+import { SharedLibModule } from '@expense-settler-monorepo/shared-lib';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, SharedLibModule],
   selector: 'expense-settler-monorepo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- libs/shared-lib/src/index.ts
+++ libs/shared-lib/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/shared-lib.module';
+export * from './lib/models/data/user.model';
