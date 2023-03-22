6249e860a8da8401c96b2e13a741549b55637622
bartlomiejwyszecki <68397602+bartlomiejwyszecki@users.noreply.github.com>
2023-03-22 21:47:35 +0100

    auth service added (#13)
    * auth service added
    
    * Local and jwt strategy files added

--- apps/es-server/src/app/auth/auth.service.ts
+++ apps/es-server/src/app/auth/auth.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUserCredentials(
+    username: string,
+    password: string
+  ): Promise<any> {
+    const user = await this.usersService.getUser({ username, password });
+
+    if (user) {
+      return user;
+    }
+
+    return null;
+  }
+
+  async loginWithCredentials(user: any) {
+    const payload = { username: user.username, sub: user._id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
