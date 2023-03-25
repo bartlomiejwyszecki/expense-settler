8cefd474c1345ecea80a9ba497f9e84196b60e70
unknown <bartlomiej.wyszecki2@gmail.com>
2023-03-25 21:32:35 +0100

    es-server removed

--- apps/es-server/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { IUser, User } from '@expense-settler-monorepo/shared-lib';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getData(): IUser[] {
-    return [];
-  }
-}
